c24c2f05bcb70ba472d85118e1fcb68905dc5870
alvynwira <alvynwira@student.telkomuniversity.ac.id>
2024-12-04 12:10:25 +0700

    create.blade.php

--- Praktikum Modul12/resources/views/employee/create.blade.php
+++ Praktikum Modul12/resources/views/employee/create.blade.php
@@ -32,8 +32,7 @@ scale=1.0">
 
                 <hr class="d-lg-none text-white-50">
 
-                <a href="{{ route('profile') }}" class="btn btn-outline
-light my-2 ms-md-auto"><i
+                <a href="{{ route('profile') }}" class="btn btn-outline light my-2 ms-md-auto"><i
                         class="bi-person-circle me-1"></i> My
                     Profile</a>
             </div>
@@ -42,6 +41,7 @@ light my-2 ms-md-auto"><i
 
     <div class="container-sm mt-5">
         <form action="{{ route('employees.store') }}" method="POST">
+            
             @csrf
             <div class="row justify-content-center">
                 <div class="p-5 bg-light rounded-3 border col-xl-6">
@@ -50,58 +50,62 @@ light my-2 ms-md-auto"><i
                         @foreach ($errors->all() as $error)
                             <div class="alert alert-danger alert
 dismissible fade show">
-                                {{ $error }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
+                               {{ $error }}
+                               <button type="button" class="btn-close"
+data-bs-dismiss="alert" aria-label="Close"></button>
                             </div>
                         @endforeach
                     @endif --}}
-
 
                     <div class="mb-3 text-center">
                         <i class="bi-person-circle fs-1"></i>
                         <h4>Create Employee</h4>
                     </div>
                     <hr>
+
                     <div class="row">
                         <div class="col-md-6 mb-3">
-                            <label for="firstName" class="form
-label">First Name</label>
-                            <input class="form-control" type="text" name="firstName" id="firstName" value=""
+                            <label for="firstName" class="form label">First Name</label>
+                            <input class="form-control @error('firstName') is-invalid @enderror" type="text"
+                                name="firstName" id="firstName" value="{{ old('firstName') }}"
                                 placeholder="Enter First Name">
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="lastName" class="form-label">Last
-                                Name</label>
-                            <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{ old('lastName')}}"
-                                placeholder="Enter Last Name">
-                                @error('lastName')
+                            @error('firstName')
                                 <div class="text-danger"><small>{{ $message }}</small></div>
-                                @enderror
+                            @enderror
                         </div>
                         <div class="col-md-6 mb-3">
-                            <label for="email" class="form
-label">Email</label>
-                            <input class="form-control" type="text" name="email" id="email" value=""
-                                placeholder="Enter Email">
+                            <label for="lastName" class="form label">Last Name</label>
+                            <input class="form-control  @error('lastName') is-invalid @enderror" type="text"
+                                name="lastName" id="email" value="{{ old('Last Name') }}" placeholder="Enter Last Name">
+                            @error('lastName')
+                                <div class="text-danger"><small>{{ $message }}</small></div>
+                            @enderror
                         </div>
                         <div class="col-md-6 mb-3">
-                            <label for="age" class="form
-label">Age</label>
-                            <input class="form-control" type="text" name="age" id="age" value=""
-                                placeholder="Enter Age">
+                            <label for="email" class="form label">Email</label>
+                            <input class="form-control  @error('email') is-invalid @enderror" type="text"
+                                name="email" id="email" value="{{ old('Email') }}" placeholder="Enter Email">
+                            @error('email')
+                                <div class="text-danger"><small>{{ $message }}</small></div>
+                            @enderror
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="age" class="form label">Age</label>
+                            <input class="form-control  @error('age') is-invalid @enderror" type="text"
+                                name="age" id="age" value="{{ old('Age') }}" placeholder="Enter Age">
+                            @error('age')
+                                <div class="text-danger"><small>{{ $message }}</small></div>
+                            @enderror
                         </div>
                     </div>
                     <hr>
                     <div class="row">
                         <div class="col-md-6 d-grid">
                             <a href="{{ route('employees.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i
-                                    class="bi-arrow-left-circle
-me-2"></i> Cancel</a>
+                                    class="bi-arrow-left-circle me-2"></i> Cancel</a>
                         </div>
                         <div class="col-md-6 d-grid">
-                            <button type="submit" class="btn btn-dark
-btn-lg mt-3"><i class="bi-check-circle me-2"></i>
+                            <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i>
                                 Save</button>
                         </div>
                     </div>
